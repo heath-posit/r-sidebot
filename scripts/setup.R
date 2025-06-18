e075fc7602e34f70f457d25d5a54d9e5e67d9339
McCrea Cobb <mccrea_cobb@fws.gov>
2025-06-18 10:10:06 -0800

    Update manifest.json. Comment out setup.R.

--- scripts/setup.R
+++ scripts/setup.R
@@ -1,15 +1,15 @@
-library(duckdb)
-library(DBI)
-library(here)
-
-db_path <- here("dat.duckdb")
-
-# Delete if exists
-if (file.exists(db_path)) {
-  unlink(db_path)
-}
-
-# Load tips.csv into a table named `tips`
-conn <- dbConnect(duckdb(), dbdir = db_path)
-duckdb_read_csv(conn, "dat", here("dat_2023.csv"))
-dbDisconnect(conn)
+# library(duckdb)
+# library(DBI)
+# library(here)
+#
+# db_path <- here("dat.duckdb")
+#
+# # Delete if exists
+# if (file.exists(db_path)) {
+#   unlink(db_path)
+# }
+#
+# # Load tips.csv into a table named `tips`
+# conn <- dbConnect(duckdb(), dbdir = db_path)
+# duckdb_read_csv(conn, "dat", here("dat_2023.csv"))
+# dbDisconnect(conn)
